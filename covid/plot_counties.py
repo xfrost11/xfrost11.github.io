a62446599d321c9d00403cc63a085170880514d4
Justin Frost <justin.frost@gmail.com>
2020-11-16 15:46:33 -0800

    Update 11/16/2020

--- covid/plot_counties.py
+++ covid/plot_counties.py
@@ -9,6 +9,7 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 cdf = pd.read_csv(r'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+hospital_df = pd.read_csv(r'https://opendata.arcgis.com/datasets/1044bb19da8d4dbfb6a96eb1b4ebf629_0.csv')
 
 population_path = os.path.abspath(r'population.xlsx')
 pdf = pd.read_excel(population_path)
@@ -23,6 +24,7 @@ mdf = pd.merge(cdf, pdf_fixed,
 mdf['per_100k'] = mdf['cases'] / mdf['POPESTIMATE2019'] * 100000
 
 print (mdf)
+mean_len = 14
 
 doc = yattag.Doc()
 with doc.tag("body"):    
@@ -49,7 +51,8 @@ with doc.tag("body"):
                             county_df = state_df[state_df['county'] == county]
                             # county_df['per_100k_ra'] = county_df['per_100k'].rolling(7).mean()
                             county_df['new_cases'] = county_df['per_100k'].diff()
-                            county_df['new_cases_ra'] = county_df['new_cases'].rolling(14).mean()
+                            if len(county_df) > mean_len:
+                                county_df['new_cases_ra'] = county_df['new_cases'].rolling(mean_len).mean()
                             county_path = os.path.join(state_path, county)
 
                             print (county_path)
@@ -59,7 +62,8 @@ with doc.tag("body"):
                             ax = plt.gca()
 
                             county_df.plot(x='date', y='new_cases', ax=ax, color = 'blue')
-                            county_df.plot(x='date', y='new_cases_ra', ax=ax, color = 'black')
+                            if len(county_df) > mean_len:
+                                county_df.plot(x='date', y='new_cases_ra', ax=ax, color = 'black')
                             plt.title(f'{state} - {county}')
 
                             plt.xlabel('')
@@ -75,12 +79,14 @@ with doc.tag("body"):
                             plt.axhspan(ymin, 1, color = "yellow", alpha = 0.5)
 
                             #plt.axhline(y = county_df['new_cases'].iloc[-1], linestyle='dashed', color = 'blue')
-                            plt.axhline(y = county_df['new_cases_ra'].iloc[-1], linestyle='dashed', color = 'black')
-                            plt.axhline(y = county_df['new_cases_ra'].iloc[-7], linestyle='dotted', color = 'black')
+                            if len(county_df) > mean_len:
+                                plt.axhline(y = county_df['new_cases_ra'].iloc[-1], linestyle='dashed', color = 'black')                            
+                                plt.axhline(y = county_df['new_cases_ra'].iloc[-7], linestyle='dotted', color = 'black')
 
                             yticks = list(plt.yticks()[0])
-                            yticks.append(county_df['new_cases_ra'].iloc[-1])
-                            yticks.append(county_df['new_cases_ra'].iloc[-7])
+                            if len(county_df) > mean_len:
+                                yticks.append(county_df['new_cases_ra'].iloc[-1])
+                                yticks.append(county_df['new_cases_ra'].iloc[-7])
 
                             yticks = sorted(yticks)
                             plt.yticks(yticks)
@@ -92,7 +98,7 @@ with doc.tag("body"):
                             
                             
                             plt.savefig(f'{county_path}.png')      
-                            state_doc.line('a', county, f'#{county}')
+                            state_doc.line('a', county, id=f'#{county}')
                             state_doc.stag('img', src=f'{county}.png')  
                             # plt.show()
                             plt.clf()
